4a8f371986e1c6ef194f2520b5cc5eab95753e78
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-01-07 19:36:06 +0300

    first commit

---
+++ mediumclone_nestjs/src/ormconfig.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import { DataSourceOptions } from "typeorm";
+
+const config: DataSourceOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'mediumclone',
+    password: '123',
+    database: 'mediumclone',
+    entities: [path.join(__dirname + '/**/*.entity{.ts, .js}')],
+    synchronize: false,
+
+};
+
+
+
+export default config;
