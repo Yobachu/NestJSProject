0af7f5de24a7a72e5502180ecd1f5dd72c771e74
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-01-28 02:10:06 +0300

    РеализоватьМетодРегистрацииПользователя

--- mediumclone_nestjs/src/user/user.controller.ts
+++ mediumclone_nestjs/src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { UserEntity } from "./user.entity";
 import { UserService } from "./user.service";
 
 @Controller()
 export class UserController{
     constructor(private readonly userService: UserService){}
     @Post('users')
-    async createUser(@Body('user') createUserDTO: any): Promise<any>{
-        console.log('createUserDTO', createUserDTO)
-        return this.userService.createUser()
+    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity>{
+        return this.userService.createUser(createUserDto)
     }
+    
 }
 
