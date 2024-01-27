9f725460611cb9b3e57969812835c5f91fa954f7
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-01-28 00:24:25 +0300

    ПодготовитьЮзера

--- mediumclone_nestjs/src/app.module.ts
+++ mediumclone_nestjs/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormconfig) , 
     TagModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ mediumclone_nestjs/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+
+@Controller()
+export class UserController{
+    constructor(private readonly userService: UserService){}
+    @Post('users')
+    async createUser(@Body('user') createUserDTO: any): Promise<any>{
+        console.log('createUserDTO', createUserDTO)
+        return this.userService.createUser()
+    }
+}
+
