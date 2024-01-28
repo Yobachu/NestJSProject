cb436ef550dcd84168a5a112e02268089ab32c43
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-01-28 19:58:55 +0300

    СгенерироватьТокенПользователя

--- mediumclone_nestjs/src/user/user.controller.ts
+++ mediumclone_nestjs/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
+import { UserResponseInterface } from "./types/userResponse.interface";
 import { UserEntity } from "./user.entity";
 import { UserService } from "./user.service";
 
@@ -7,8 +8,9 @@ import { UserService } from "./user.service";
 export class UserController{
     constructor(private readonly userService: UserService){}
     @Post('users')
-    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity>{
-        return this.userService.createUser(createUserDto)
+    async createUser(@Body('user') createUserDto: CreateUserDto,): Promise<UserResponseInterface>{
+        const user = await this.userService.createUser(createUserDto);
+        return this.userService.buildUserResponse(user)
     }
     
 }
