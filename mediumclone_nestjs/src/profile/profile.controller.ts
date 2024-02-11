238dfe557c3848cc793f011bcafc319241b9a753
Yobachu <87315049+Yobachu@users.noreply.github.com>
2024-02-12 02:04:25 +0300

    ИмплементироватьФоллоуПользователей

--- mediumclone_nestjs/src/profile/profile.controller.ts
+++ mediumclone_nestjs/src/profile/profile.controller.ts
@@ -1,5 +1,6 @@
 import { User } from "@app/user/decorators/user.decorators";
-import { Controller, Get, Param } from "@nestjs/common";
+import { AuthGuard } from "@app/user/guards/auth.gurd";
+import { Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { ProfileService } from "./profile.service";
 import { ProfileResponseInterface } from "./types/profileResponse.interface";
 
@@ -9,6 +10,13 @@ export class ProfileController{
     @Get(':username')
     async getProfile(@User('id') currentUserId: number, @Param('username') profileUsername: string): Promise<ProfileResponseInterface>{
         const profile =  await this.profileService.getProfile(currentUserId, profileUsername)
+        return this.profileService.buildProfileResponse(profile)
+    }
+
+    @Post(':username/follow')
+    @UseGuards(AuthGuard)
+    async followProfile(@User('id') currentUserId: number, @Param('username') profileUsername: string): Promise <ProfileResponseInterface>{
+        const profile =  await this.profileService.followProfile(currentUserId, profileUsername)
         return this.profileService.buildProfileResponse(profile)
     }
 }
